696685fff91aba99c8a6afcffde14ef48f7cf728
Nigmat <harrynick005@gmail.com>
2025-03-14 05:11:48 +0800

    master branch push

--- src/main/java/com/cydeo/TicketingProjectMVC.java
+++ src/main/java/com/cydeo/TicketingProjectMVC.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketingProjectMVC {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketingProjectMVC.class, args);
+         SpringApplication.run(TicketingProjectMVC.class, args);
     }
 
 }
